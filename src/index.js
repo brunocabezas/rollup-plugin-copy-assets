f250b816833f995be198976b8b35ee2d9bc9604c
Bruno Cabezas <bruno.cabezas@gmail.com>
2018-12-27 10:46:33 -0300

    compatible with input as array on rollup cfg, new outputDir config param

--- src/index.js
+++ src/index.js
@@ -9,20 +9,35 @@ import path from 'path';
  * @return {Object} The rollup code object.
  */
 export default function copy(options = {}) {
-  const { assets } = options;
+  const { assets, outputDir } = options;
   let basedir = '';
   return {
     name: 'copy-assets',
     options: function options(options) {
       // Cache the base directory so we can figure out where to put assets.
-      basedir = path.dirname(options.input);
+      // Handling input as array
+      const inputStr = Array.isArray(options.input)
+        ? options.input[0]
+        : options.input;
+      basedir = path.dirname(inputStr);
     },
     generateBundle: ({ file, dir }) => {
       const outputDirectory = dir || path.dirname(file);
-      return Promise.all(assets.map((asset) => fs.copy(
-        asset,
-        path.join(outputDirectory, path.relative(basedir, asset))
-      )));
+      return Promise.all(
+        assets.map((asset) => {
+          // console.log(!!outputDir, outputDir, dir);
+          const isFolder = !path.basename(asset).includes('.');
+          const out = !!outputDir
+            ? path.join(
+                outputDirectory,
+                outputDir,
+                isFolder ? '' : path.basename(asset)
+              )
+            : path.join(outputDirectory, path.relative(basedir, asset));
+          console.log(out);
+          return fs.copy(asset, out);
+        })
+      );
     },
   };
 }
